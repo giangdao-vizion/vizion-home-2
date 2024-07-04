2495772a86de176aa4216fe6cf756d55d5325087
Dinh Pham <dinh.pham@successsoftware.local>
2024-07-04 09:28:35 +0700

    #patched update minor UI color

--- components/vizion/CommonModules/TourIframe/TourIframe.jsx
+++ components/vizion/CommonModules/TourIframe/TourIframe.jsx
@@ -4,38 +4,19 @@ export default function TourIframe({ iframeUrl }) {
       <div className="section spdt mb60">
         <div className="container">
           <div className="heading align-center">
-            <div className="heading-sub color-vizion">Khám Phá</div>
-            <h2 className="heading-title size-l">
-              Trải Nghiệm Ngay <span className="heading-title-hightlight color-vizion">Live Demo</span>
-            </h2>
-          </div>
-          <div>
-            {/* <div
-              className="popup-wrapper ratio br20"
-              style={{ aspectRatio: 4 / 2 }}
-            >
-              <img src="/assets/images/nft-11.jpg" alt="Popup Video" />
-              <a
-                onClick={() => setOpen(true)}
-                className="icon-popup background-navy size-l"
-              >
-                <i className="las la-play" />
-              </a>
+            <h2 className="heading-title size-l">Khám phá</h2>
+            <div className="heading-desc">
+              Trải nghiệm ngay <span className="vz-span">Live Demo</span>.
             </div>
-            <ModalVideo
-              channel="youtube"
-              autoplay
-              isOpen={isOpen}
-              videoId="vfhzo499OeA"
-              onClose={() => setOpen(false)}
-            /> */}
-            <iframe
-              className="homepage-iframe"
-              src={iframeUrl}
-              title="tour-immerse"
-              style={{ height: 540, width: '100%' }}
-            />
           </div>
+        </div>
+        <div className="iframe-container">
+          <iframe
+            className="homepage-iframe"
+            src={iframeUrl}
+            title="tour-immerse"
+            style={{ height: 540, width: '100%' }}
+          />
         </div>
       </div>
     </>
