c944920b01c2cd7ecf0926db26c5ef2a0a32e604
vuduong-vizion <vu.duong@vizion.space>
2024-07-04 09:39:19 +0700

    update loop video.

--- components/vizion/Home/Banner.jsx
+++ components/vizion/Home/Banner.jsx
@@ -4,8 +4,7 @@ export default function Banner() {
       <section className="section opt120 overflow-hidden linear-gradient-07 banner">
         <div className="banner-video">
           <iframe
-            // src="https://www.youtube.com/embed/ZlDUvmBOGOE?si=JxkfoEbOGAwlyjxE&autoplay=1&mute=1&loop=1&controls=0"
-            src="https://www.youtube.com/embed/bXlQ3Mw4uGc?si=Izi3mZObD6T5zKY8&autoplay=1&mute=1&loop=1&controls=0&playlist=bXlQ3Mw4uGc"
+            src="https://www.youtube.com/embed/ZlDUvmBOGOE?si=9mqZxqM-MkhpRNZf&autoplay=1&mute=1&loop=1&controls=0&playlist=ZlDUvmBOGOE"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
